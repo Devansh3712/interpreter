1334a614e72a8b1b90f04da3e775ff0e79f53a95
Devansh Singh <devanshamity@gmail.com>
2024-09-17 14:51:44 +0530

    Evaluate if-else conditions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -107,6 +107,27 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	}
 }
 
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case FALSE:
+		return false
+	default:
+		return true
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	}
+	return NULL
+}
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -126,7 +147,10 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
-
 	return nil
 }
